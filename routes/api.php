605bd6e1d158b70ca2c51da564bc02fda384fa09
pasukanbocil <ctian8787@gmail.com>
2024-05-05 17:50:57 +0700

    refactor: Remove commented out code and add user route with placeholder function

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,19 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('users/{id}', function ($id) {
+
+});
+
+// Route::get('users', function () {
+
+// });
+
+// Route::post('users', function () {
+
+// });
+
+// Route::put('users/{id}', function ($id) {
+
+// });
